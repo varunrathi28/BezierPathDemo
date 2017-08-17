3c4f995d0e4cc4bcd80fc3416ddc9ab1a645a700
Varun Rathi <varunrathi28@gmail.com>
2017-08-17 18:36:22 +0530

    Function for Rectange Added

---
+++ BezierPathDemo/DemoView.swift
@@ -0,0 +1,56 @@
+//
+//  DemoView.swift
+//  BezierPathDemo
+//
+//  Created by Varun Rathi on 17/08/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class DemoView: UIView {
+
+    var path:UIBezierPath!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    func createRectange()
+    {
+         path = UIBezierPath()
+        
+        // Move to origin
+         path.move(to: CGPoint(x: 0.0, y: 0.0))
+        
+        
+        // Draw line to bottom left corner
+        path.addLine(to: CGPoint(x: 0, y: self.frame.size.height))
+        
+        // Draw from bottom left to bottom right
+        
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        
+        // Draw
+        
+        
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ BezierPathDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  BezierPathDemo
+//
+//  Created by Varun Rathi on 17/08/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let width:CGFloat = 240.0
+        let height:CGFloat = 160.0
+        
+        let demoView = DemoView(frame: CGRect(x: self.view.frame.size.width/2, y:  self.view.frame.size.height/2, width: width, height: height))
+        self.view.addSubview(demoView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
